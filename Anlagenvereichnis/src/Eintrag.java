6002b5c042795f528b0dd8e6778de33ab530e3e5
riemab16 <riemab16@new.edvhtl.at>
2018-10-10 11:37:56 +0200

    Anlage angefangen

--- Anlagenvereichnis/src/Eintrag.java
+++ Anlagenvereichnis/src/Eintrag.java
@@ -1,8 +1,26 @@
 
 public class Eintrag {
+    
     private String bezeichnung;
     private int ankauf;
     private double inbDatum;
     private double nutzungsdauer;
     private double bisherND;
+    private double afaBisher;
+    private double wertVor;
+    private double afaDJ;
+    private double WB;
+    
+     public Eintrag(String bezeichnung, int ankauf, double inbDatum, double nutzungsdauer, double bisherND, double afaBisher, double wertVor, double afaDJ, double WB) {
+        this.bezeichnung = bezeichnung;
+        this.ankauf = ankauf;
+        this.inbDatum = inbDatum;
+        this.nutzungsdauer = nutzungsdauer;
+        this.bisherND = bisherND;
+        this.afaBisher = afaBisher;
+        this.wertVor = wertVor;
+        this.afaDJ = afaDJ;
+        this.WB = WB;
+    }
+    
 }
